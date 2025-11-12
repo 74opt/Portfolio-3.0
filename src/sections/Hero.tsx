3cb84ba2b111921ce66400a229363ec1ad1c4eb4
74opt <matthieuderobles@gmail.com>
2025-11-11 22:02:01 -0500

    Prepped for importing models

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import HeroExperience from '../components/HeroModels/HeroExperience.tsx';
 
 const Hero = () => {
     return (
         <section id="hero" className="relative overflow-hidden">
             <div className="absolute top-0 left-0 z-10">
-                
+                {/* Background image (would want a gradient sky bg here) */}
             </div>
+
+            <div className="hero-layout">
+                {/* LEFT: HERO CONTENT */}
+                <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+                    <div className="flex flex-col gap-7">
+                        <div className="hero-text">
+                            <h1>Title</h1>
+                        </div>
+                    </div>
+                </header>
+                {/* 3D Model */}
+                <figure>
+                    <div className="hero-3d-layout">
+                        <HeroExperience />
+                    </div>
+                </figure>
+            </div>
+
         </section>
     )
 }
